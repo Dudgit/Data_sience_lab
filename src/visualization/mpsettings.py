2666e57f0311e29251d20b04e5ce0af053783da8
Dudgit <52243953+Dudgit@users.noreply.github.com>
2021-10-04 16:24:40 +0200

    Eda updated
    Grouping added with pandas. Also matplotlib settings added, and enviroment.yml updated

---
+++ src/visualization/mpsettings.py
@@ -0,0 +1,12 @@
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+plt.style.use("dark_background")
+
+matplotlib.rcParams["figure.figsize"] = (8,8)
+matplotlib.rcParams["axes.titlesize"] = 20
+matplotlib.rcParams["axes.labelsize"] = 20
+matplotlib.rcParams["axes.grid"] = True
+matplotlib.rc('lines',linewidth = 2)
